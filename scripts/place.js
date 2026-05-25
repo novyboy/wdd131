1fa6183f6678fb0dedc05a8e5c7cafc10dc7b5a2
novyboy <novakaiisland+github@gmail.com>
2026-05-25 15:32:41 +0900

    function update

--- scripts/place.js
+++ scripts/place.js
@@ -1,13 +1,6 @@
 const temperature = 75;
 const windSpeed = 7;
-function calculateWindChill(temperature, windSpeed) {//fahrenheit
-	const v16 = windSpeed ** 0.16;
-	const windChill = 35.74 + 
-	0.6215 * temperature -
-	35.75 * v16 +
-	0.4275 * temperature * v16;
-	return windChill;
-}
+const calculateWindChill = (temperature, windSpeed) => 35.74 + 0.6215 * temperature - 35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed ** 0.16;
 
 document.getElementById("temperature").innerText = `${temperature}°F`;
 document.getElementById("conditions").innerText = "Sunny";
