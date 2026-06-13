a09d6082f4feb27212cba2bb6d803def234b319b
novyboy <novakaiisland+github@gmail.com>
2026-06-13 12:28:03 +0900

    review display

--- scripts/review.js
+++ scripts/review.js
@@ -1,4 +1,4 @@
 const count = Number(localStorage.getItem("reviewCount")) || 0;
 localStorage.setItem("reviewCount", count + 1);
 
-console.log("Review Count:", count);
+document.getElementById("reviewCount").innerHTML = `${count} Reviews Submitted`;
